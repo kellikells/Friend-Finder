8d91da2e6c2cda59d5345c5058139e1d54e8136b
kelli <kellikells@gmail.com>
2020-03-19 01:00:39 -0700

    friends.js has empty variable to hold submission of data from survey.html

---
+++ app/data/friends.js
@@ -0,0 +1,12 @@
+var usersArray = [
+    {
+        'name': '',
+        'photo': '',
+        'scores':[]
+    }
+]
+
+// ===== EXPORTING THE ARRAY ======
+// --- so it can be required to compare with incoming user data 
+// --- and for display in response to survey submission
+module.exports = usersArray;
