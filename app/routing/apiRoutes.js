08b6fb1f8f849e4964d949b6faa896abf67fa81c
kelli <kellikells@gmail.com>
2020-03-21 19:22:46 -0700

    script in survey.html calculates the total score for the current user to compare with data. post to api of survey works

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,5 @@
-var path = require('path');
 
 // ============== 1- LOADING DATA ================
-
 
 var friends = require('../data/friends');
 
@@ -11,15 +9,34 @@ var friends = require('../data/friends');
 
 module.exports = function (app) {
 
-
-    // ====== API GET: display JSON of entire data  
-
+    // ----- API GET requests: display JSON of entire data  
     app.get('/api/friends', function (req, res) {
         res.json(friends);
+    });
+
+    // ----- API POST requests: 
+    // handles survey results & compatibility logic
+    app.post('/api/friends', function (req, res) {
+
+        // adding the user's survey to all the data
+        friends.push(req.body);
+
+        res.json;
+
+
+
+
+    });
+
+    app.post("/api/clear", function (req, res) {
+        // Empty out the arrays of data
+        // tableData.length = 0;
+        waitListData.length = 0;
+
+        res.json({ ok: true });
     });
 }
 
 
 
-// ====== POST      /api/frieds
-//        handles survey results & compatibility logic
+
