521c2522c56c4611d6fc64b97cda54d03ef36cf2
kelli <kellikells@gmail.com>
2020-03-29 15:04:32 -0700

    changed logic and now returns closest friend match if no exact match

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -23,21 +23,32 @@ module.exports = function (app) {
 
         // saving index of the user's data
         var x = (friends.length - 1);
+        var dif =1000;
+        var user = {};
 
-        for (let i = 0; i < friends.length; i++) {
+
+
+        for (let i = 0; i < friends.length-1; i++) {
+            
 
             var difference = Math.abs(friends[x].scoreTotal - (friends[i].scoreTotal));
-
-            if (difference === 0) {
-                console.log(`friend match: ${friends[i].name}`);
-                console.log(`user is: ${friends[x].name}`);
-                res.json(friends[i]);
-                return;
-            } else {
-                res.json("no match");
-                return;
+            if (dif > difference) {
+                dif = difference;
+                user = friends[i];
             }
+
+            // if (difference === 0) {
+            //     console.log(`friend match: ${friends[i].name}`);
+            //     console.log(`user is: ${friends[x].name}`);
+            //     res.json(friends[i]);
+            //     return;
+            // } else {
+            //     res.json("no match");
+            //     return;
+            // }
         }
+        console.log(user);
+        res.json(user);
     });
 
     // Empty out the arrays of data
